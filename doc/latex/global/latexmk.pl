cd752847638947bc87980b0efabb8fcdd8e92031
Nicolas Martin <nicolas.martin@sorbonne-universite.fr>
2022-02-02 12:14:24 +0100

    Fixes for latex font and latemk custom makeindex cmd

--- doc/latex/global/latexmk.pl
+++ doc/latex/global/latexmk.pl
@@ -10,7 +10,7 @@ $out_dir            = '../build';
 ## Custom cmds
 set_tex_cmds('-shell-escape -file-line-error -interaction=nonstopmode');
 #set_tex_cmds('-shell-escape -file-line-error');
-$makeindex = 'makeindex %O -s %R.ist -o %D %S';
+$makeindex = "makeindex %O -s ../../global/index.ist -o $out_dir/%D $out_dir/%S";
 ## %D: Destination file (.ind for index)
 ## %O: Options
 ## %R: Root filename (${model}_manual)
